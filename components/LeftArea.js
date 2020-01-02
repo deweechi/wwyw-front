6a34217cd48b17395c6d9e08b1afcfbb60e7e092
deweechi <chipdeweese@gmail.com>
2020-01-02 07:57:33 -0500

    Add Left Area to page.
    Signed-off-by: deweechi <chipdeweese@gmail.com>

---
+++ components/LeftArea.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LeftArea = () => {
+    
+    
+    return (
+        <div>
+            <p>Thank you for visiting What Wood You Wish!</p>
+            <p>All items are hand made in the USA</p>
+            <p>All items are hand made in the USA</p>
+        </div>
+    );
+};
+
+export default LeftArea;<p>I am the left area!!!!!!!!</p>
